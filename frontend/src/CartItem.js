df3619ad4fc17a7841138c9b6165e15a8fd069e0
joshishaswat <joshishaswat@gmail.com>
2021-07-31 19:22:17 -0400

    updated delivery time

--- frontend/src/CartItem.js
+++ frontend/src/CartItem.js
@@ -102,8 +102,6 @@ function CartItem({ itemId, qty: quantity, delivery_time: dtime, refreshCart, sp
                     if (itemId === "60dfa9e2bf70ac0017547f2f" || itemId === "60dfa9b6bf70ac0017547f2e") { //cakes and bakes
                       return ( 
                         <React.Fragment>
-                          <option> Saturday, July 31 </option>
-                          <option> Sunday, August 01 </option>
                           <option> Saturday, August 07 </option>
                           <option> Sunday, August 08 </option>
                         </React.Fragment>
